0466adfd8ebc6d579d3c01b47fdd721c2789efbd
Meeri <meeri.piispa@helsinki.fi>
2026-09-02 15:01:24 +0300

    Add comment on prophage quality retention
    Added a comment about quality retention criteria for prophages.

--- Scripts/02_checkv.sh
+++ Scripts/02_checkv.sh
@@ -2,3 +2,5 @@ checkv end_to_end \
     viral_sequences.fna \
     checkv_output \
     -t 16
+
+#Only complete, high-quality, and medium-quality (>50% completeness) prophages were retained for downstream analyses.
